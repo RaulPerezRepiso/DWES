41d22f62c5321850928b781f930a1070e786f63d
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-10-08 14:56:12 +0200

    Ejercicio 2 Empezado  08/10/2025

--- PrimerTrimestre/aplicacion/practica2/ejercicio2.php
+++ PrimerTrimestre/aplicacion/practica2/ejercicio2.php
@@ -8,25 +8,47 @@ $ubicacion = [
     "Ejercicio 2" => "ejercicio2.php"
 ];
 $GLOBALS['ubicacion'] = $ubicacion;
+
+$cadena = "Está la niña en casa";
+
 inicioCabecera("EJERCICO_2");
 
 inicioCuerpo("Ejercicio 2: Lanzamiento de un dado");
 
-$cadena = "Está la niña en casa";
-$array = str_split($cadena, 1);
-
-foreach($array as $valor){
-    echo $valor;
-}
-
-
-cuerpo();
+cuerpo($cadena);
 finCuerpo();
 ?>
 
 <?php
-function cuerpo()
+function cuerpo($cadena)
 {
-    
-}
+?>
+    <!--Cadena spliteadas  -->
+    <h2>Prueba mb_str_split</h2>
+    <?php foreach (mb_str_split($cadena) as $i => $valor) { ?>
+        <p>
+            <?= str_repeat("&nbsp;", $i) . htmlspecialchars($valor) ?>
+        </p>
+    <?php } ?>
+    <h2>Prueba mb_substr</h2>
+    <?php for ($i = 0; $i < mb_strlen($cadena); $i++) { ?>
+        <p>
+            <?= str_repeat("&nbsp;", $i) . mb_substr($cadena, $i, 1) ?>
+        </p>
+    <?php } ?>
+    <h2>Prueba substr</h2>
+    <?php for ($i = 0; $i < strlen($cadena); $i++) { ?>
+        <p>
+            <?= str_repeat("&nbsp;", $i) . substr($cadena, $i, 1) . "<br>" ?>
+        </p>
+    <?php } ?>
 
+    <!--Cadena inversas spliteadas  -->
+    <h2>mb_substr con orden inverso y alteración de cadena</h2>
+    <?php for ($i = mb_strlen($cadena); mb_strlen($cadena) >= 0; $i--) { ?>
+        <p>
+            <?= str_repeat("&nbsp;", $i) . mb_substr($cadena, $i, 1) ?>
+        </p>
+    <?php } ?>
+<?php
+}
