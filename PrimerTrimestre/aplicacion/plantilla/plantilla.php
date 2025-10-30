00f38bd1ff17b57243bc143b5ccfb99ad1a9e81b
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-10-30 14:08:17 +0100

    Relación 5. Ejercico 2 Medio Acabado. 30/10/2025

--- PrimerTrimestre/aplicacion/plantilla/plantilla.php
+++ PrimerTrimestre/aplicacion/plantilla/plantilla.php
@@ -55,12 +55,12 @@ function inicioCuerpo(string $cabecera)
                 <div id="menuPrincipal">
                     <ul>
                         <li><a href="/index.php">Inicio</a></li>
-                        <li><a href="/aplicacion/practica1/index.php">Práctica 1</a></li>
-                        <li><a href="/aplicacion/practica2/index.php">Práctica 2</a></li>
-                        <li><a href="/aplicacion/practica3/index.php">Práctica 3</a></li>
-                        <li><a href="/aplicacion/practica4/index.php">Práctica 4</a></li>
-                        <li><a href="/aplicacion/practica5/index.php">Práctica 5</a></li>
                         <li><a href="/aplicacion/pruebas/index.php">Pruebas</a></li>
+                        <li><a href="/aplicacion/practica1/index.php">Relación I</a></li>
+                        <li><a href="/aplicacion/practica2/index.php">Relación II</a></li>
+                        <li><a href="/aplicacion/practica3/index.php">Relación III</a></li>
+                        <li><a href="/aplicacion/practica4/index.php">Relación IV</a></li>
+                        <li><a href="/aplicacion/practica5/index.php">Relación V</a></li>
                     </ul>
                 </div>
             </header>
--- PrimerTrimestre/aplicacion/practica5/index.php
+++ PrimerTrimestre/aplicacion/practica5/index.php
@@ -1,6 +1,8 @@
 <?php
 include_once(dirname(__FILE__) . "/../../cabecera.php");
 include_once("/Users/2daw/DWES/PrimerTrimestre/scripts/librerias/validacion.php");
+include_once("/Users/2daw/DWES/PrimerTrimestre/aplicacion/practica5/Formulario.php");
+
 // Barra de ubicación para la página índice
 $ubicacion = [
     "Index Principal" => "/index.php",
@@ -26,9 +28,179 @@ function cuerpo()
 ?>
     <h1>Relación 5: Introducción de información.</h1>
 <?php
-    $var = 1;
-    $resultado = validaEntero($var, 5, 7, 7);
+    $formulario = new Formulario();
+    $formulario->procesarDatos();
+}
 
-    echo "Resultado: " . ($resultado ? "válido" : "inválido") . "<br>";
-    echo "Valor final: $var";
+include_once("/Users/2daw/DWES/PrimerTrimestre/scripts/librerias/validacion.php");
+
+class Formulario
+{
+
+    //Creamos propieades privadas que guarden todo en el array
+    private array $datos;
+    private array $errores;
+
+    public function __construct()
+    {
+        $this->datos = [
+            "nombre" => "",
+            "password" => "",
+            "fechaNacimiento" => "",
+            "fechaCarnet" => [
+                "dia" => "",
+                "mes" => "",
+                "anio" => ""
+            ],
+            "hora" => "",
+            "estado" => "",
+            "estudios" => [],
+            "hermanos" => 0,
+            "sueldo" => 1100
+
+        ];
+
+        $this->errores = [];
+    }
+
+    public function procesarDatos(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->recogerDatos();
+            $this->validarDatos();
+
+            if ($this->errores) {
+                $this->mostrarFormulario();
+            } else {
+                $this->mostrarResumen();
+            }
+        } else {
+            $this->mostrarFormulario();
+        }
+    }
+
+    public function recogerDatos(): void
+    {
+        //Nombre
+        // $this->datos["datos"] = $_POST["nombre"] ?? "";
+        if (isset($_POST["nombre"])) {
+            $nombre = trim($_POST["nombre"]);
+            $this->datos["nombre"] = $nombre;
+        } else {
+            $this->datos["nombre"] = "";
+        }
+
+        // Constraseña
+        if (isset($_POST["password"])) {
+            $password = trim($_POST["password"]);
+            $this->datos["password"] = $password;
+        } else {
+            $this->datos["password"] = "";
+        }
+
+        // Fecha de Nacimiento
+        if (isset($_POST["fechaNacimiento"])) {
+            $fechaNacimiento = $_POST["fechaNacimiento"];
+            $this->datos["fechaNacimiento"] = $fechaNacimiento;
+        } else {
+            $this->datos["fechaNacimiento"] = "";
+        }
+
+        // Fecha de Carnet
+        if (isset($_POST["dia"])) {
+            $this->datos["fechaCarnet"]["dia"] = trim($_POST["dia"]);
+        } else {
+            $this->datos["fechaCarnet"]["dia"] = "";
+        }
+        if (isset($_POST["mes"])) {
+            $this->datos["fechaCarnet"]["mes"] = trim($_POST["mes"]);
+        } else {
+            $this->datos["fechaCarnet"]["mes"] = "";
+        }
+        if (isset($_POST["anio"])) {
+            $this->datos["fechaCarnet"]["anio"] = trim($_POST["anio"]);
+        } else {
+            $this->datos["fechaCarnet"]["anio"] = "";
+        }
+
+        // Hora
+        if (isset($_POST["hora"])) {
+            $hora = $_POST["hora"];
+            $this->datos["hora"] = $hora;
+        } else {
+            $this->datos["hora"] = "";
+        }
+
+        // Estado
+        if (isset($_POST["estado"])) {
+            $estado = trim($_POST["estado"]);
+            $this->datos["estado"] = $estado;
+        } else {
+            $this->datos["estado"] = "";
+        }
+
+        // Estudios
+        if (isset($_POST["estudios"]) && is_array($_POST["estudios"])) {
+            $this->datos["estudios"] = $_POST["estudios"];
+        } else {
+            $this->datos["estudios"] = [];
+        }
+
+        // Hermanos
+        if (isset($_POST["hermanos"])) {
+            $hermanos = intval($_POST["hermanos"]);
+            $this->datos["hermanos"] = $hermanos;
+        } else {
+            $this->datos["hermanos"] = 0;
+        }
+
+        // Sueldo
+        if (isset($_POST["sueldo"])) {
+            $sueldo = floatval($_POST["sueldo"]);
+            $this->datos["sueldo"] = $sueldo;
+        } else {
+            $this->datos["sueldo"] = 1100;
+        }
+    }
+
+    public function validarDatos(): void
+    {
+        // Validar nombre
+        $nombre = $this->datos["nombre"];
+        if (!validaCadena($nombre, 25, "")) {
+            $this->errores["nombre"][] = "El nombre no puede estar vacío ni superar 25 caracteres.";
+        } elseif (str_starts_with(mb_strtoupper($nombre), "H")) {
+            $this->errores["nombre"][] = "El nombre no puede empezar por H.";
+        } else {
+            $this->datos["nombre"] = mb_strtoupper($nombre);
+        }
+
+        //Validar constraseña
+        $password = $this->datos["password"];
+        $defecto = "";
+        if (!validaCadena($password, 15, $defecto) || mb_strlen($password) < 8) {
+            $this->errores["password"][] = "La contraseña debe tener entre 8 y 15 caracteres.";
+        } elseif (!validaExpresion($password, '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\$_\-;<>@]).{8,15}$/', $defecto)) {
+            $this->errores["password"][] = "Debe incluir al menos una mayúscula, una minúscula, una cifra y un carácter especial (\$_-;<>@).";
+        } else {
+            $this->datos["password"] = $password;
+        }
+
+        //Validar fechaNacimiento
+        $fechaNacimiento = $this->datos["fechaNacimiento"];
+        if (!validaFecha($fechaNacimiento, $defecto)) {
+            $this->errores["fechaNacimiento"][] = "La fecha no es válida";
+        }else{
+            $this->datos["fechaNacimiento"] = $fechaNacimiento;
+        }
+        
+        //Validar fecha de Carnet
+        $fechaCarnet = $this->datos["fechaCarnet"];
+        
+
+    }
+
+    public function mostrarFormulario(): void {}
+
+    public function mostrarResumen(): void {}
 }
