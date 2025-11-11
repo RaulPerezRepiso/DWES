8129201b85c75eacce62b921941fb4857bf3a65a
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-11-11 14:49:52 +0100

    Ejericico 1. Acabado a falta de Crear Punto. 11/11/2025

--- PrimerTrimestre/aplicacion/practica7/index.php
+++ PrimerTrimestre/aplicacion/practica7/index.php
@@ -8,7 +8,41 @@ $ubicacion = [
 ];
 $GLOBALS['ubicacion'] = $ubicacion;
 
-inicioCabecera("PRÁCTICA-7");
+function Formulario()
+{
+?>
+    <h1>Relación 7: Ficheros</h1>
+
+    <form method="post">
+        <label for="x"><strong>X:</strong></label>
+        <input type="number" name="x" id="x" min="0" max="20000" required><br><br>
+
+        <label for="y"><strong>Y:</strong></label>
+        <input type="number" name="y" id="y" min="0" max="20000" required><br><br>
+
+        <label for="color"><strong>Color:</strong></label>
+        <select name="color" id="color">
+            <option value="black">Negro</option>
+            <option value="yellow">Amarillo</option>
+            <option value="blue">Azul</option>
+            <option value="green">Verde</option>
+        </select><br><br>
+
+        <label for="grosor"><strong>Grosor:</strong></label><br>
+        <label for="grosor1">Fino</label>
+        <input type="radio" name="grosor" value="1" id="grosor1">
+        <label for="grosor2">Medio</label>
+        <input type="radio" name="grosor" value="1" id="grosor2">
+        <label for="grosor3">Grueso</label>
+        <input type="radio" name="grosor" value="1" id="grosor3">
+        <br><br>
+
+        <button type="submit">Crear Punto</button>
+    </form>
+<?php
+}
+
+inicioCabecera("PRÁCTICA_7");
 cabecera();
 finCabecera();
 
@@ -23,7 +57,5 @@ function cabecera() {}
 
 function cuerpo()
 {
-?>
-    <h1>Relación 7: Ficheros</h1>
-<?php
+    echo Formulario();
 }
--- PrimerTrimestre/scripts/clases/Punto.php
+++ PrimerTrimestre/scripts/clases/Punto.php
@@ -1,6 +1,110 @@
 <?php
-class Punto{
-    
-}
+include_once("../../scripts/librerias/validacion.php");
 
-?>
+
+class Punto
+{
+    // CONSTANTES
+    public const COLORES = [
+        "black" => ["nombre" => "negro", "rgb" => [0, 0, 0]],
+        "yellow" => ["nombre" => "amarillo", "rgb" => [255, 255, 0]],
+        "blue" => ["nombre" => "azul", "rgb" => [0, 0, 255]],
+        "green" => ["nombre" => "verde", "rgb" => [0, 128, 0]]
+    ];
+
+    public const GROSORES = [
+        1 => "fino",
+        2 => "medio",
+        3 => "grueso"
+    ];
+
+    // PROPIEDADES
+    private int $x;
+    private int $y;
+    private string $color;
+    private int $grosor;
+
+    public function __construct(int $x, int $y, string $color, int $grosor)
+    {
+        $this->setX($x) ?: throw new Exception("Valor de X no válido");
+        $this->setY($y) ?: throw new Exception("Valor de Y no válido");
+        $this->setColor($color) ?: throw new Exception("Valor de Color no válido");
+        $this->setGrosor($grosor) ?: throw new Exception("Valor de Grosor no válido");
+    }
+
+    //Getter's & Setter's
+    /**
+     * Setter que valida que el valor de x este entre 0 y hasta 20000.
+     *
+     * @param int $valor
+     * @return boolean
+     */
+    public function setX(int $valor): bool
+    {
+        if (!validaEntero($valor, 0, 20000, 0)) return false;
+        $this->x = $valor;
+        return true;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    /**
+     * Setter que valida que el valor de y este entre 0 y hasta 20000.
+     *
+     * @param int $valor
+     * @return boolean
+     */
+    public function setY(int $valor): bool
+    {
+        if (!validaEntero($valor, 0, 20000, 0)) return false;
+        $this->y = $valor;
+        return true;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    /**
+     * Setter que valida que el valor exista en la constante
+     *
+     * @param string $valor
+     * @return boolean
+     */
+    public function setColor(string $valor): bool
+    {
+        if (array_key_exists($valor, self::COLORES)) {
+            $this->color = $valor;
+            return true;
+        }
+        return false;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    /**
+     * Setter que valida que el valor este en el rango de la contante
+     *
+     * @param float $valor
+     * @return boolean
+     */
+    public function setGrosor(int $valor): bool
+    {
+        if (validaRango($valor, Punto::GROSORES, 2)) {
+            $this->grosor = $valor;
+            return true;
+        } else return false;
+    }
+
+    public function getGrosor(): int
+    {
+        return $this->grosor;
+    }
+}
