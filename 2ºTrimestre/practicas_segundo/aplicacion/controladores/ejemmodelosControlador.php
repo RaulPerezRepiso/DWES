474fac851c5a79e4d8f673e5dbf6191429865dc0
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2026-01-21 14:55:21 +0100

    Creacion de Tablas con framework. 21/01/2026

--- 2ºTrimestre/practicas_segundo/aplicacion/controladores/ejemmodelosControlador.php
+++ 2ºTrimestre/practicas_segundo/aplicacion/controladores/ejemmodelosControlador.php
@@ -44,9 +44,87 @@ class ejemmodelosControlador extends CControlador
 				//Son válidos los datos
 				echo "Todo correcto";
 				return;
-
 			}
 		}
 		$this->dibujaVista("nuevo", ["modelo" => $art], "Usuarios Existentes");
+	}
+
+	public function accionTabla()
+	{
+		$filas = [
+			[
+				"nombre" => "Ramon",
+				"edad" => "22",
+				"direccion" => "BajoPuente"
+			],
+			[
+				"nombre" => "Pedro",
+				"edad" => "46",
+				"direccion" => "Caseta de Perrsos"
+			],
+			[
+				"nombre" => "Micaela",
+				"edad" => "23",
+				"direccion" => "Medica"
+			],
+			[
+				"nombre" => "Pascal",
+				"edad" => "43",
+				"direccion" => "España"
+			],
+			[
+				"nombre" => "Nathaly",
+				"edad" => "21",
+				"direccion" => "Esquina"
+			]
+		];
+
+		$cabecera = array(
+			array(
+				"ETIQUETA" => "el nombre",
+				"CAMPO" => "nombre"
+			),
+			array(
+				"ETIQUETA" => "la edad",
+				"CAMPO" => "edad"
+			),
+			array(
+				"ETIQUETA" => "la direccion",
+				"CAMPO" => "direccion"
+			)
+		);
+
+		$totalRegistros = count($filas);
+		$regPag = 5;
+		if (isset($_GET["reg_pag"]))
+			$regPag = intval($_GET["reg_pag"]);
+
+		$nPaginas = $totalRegistros / $regPag;
+		$pag = 1;
+		if(isset($_GET["pag"]))
+			$pag=intval($_GET["pag"]);
+
+		$salida=[];
+		for($cont=($pag-1)*$regPag; $cont<$pag*$regPag; $cont++)
+			$salida[]=$filas[$cont];
+
+		$opcPaginador = array(
+			"URL" => Sistema::app()->generaURL(array("ejenmodelos", "tabla")),
+			"TOTAL_REGISTROS" => $totalRegistros,
+			"PAGINA_ACTUAL" => $pag,
+			"REGISTROS_PAGINA" => $regPag,
+			"TAMANIOS_PAGINA" => array(
+				5 => "5",
+				10 => "10",
+				20 => "20",
+				30 => "30",
+				40 => "40",
+				50 => "50"
+			),
+			"MOSTRAR_TAMANIOS" => true,
+			"PAGINAS_MOSTRADAS" => 7,
+		);
+
+		$this->dibujaVista("tabla", ["fill" => $salida, "cabecera" => $cabecera, "cabPag" => $opcPaginador, "ejemplo"]);
 	}
 }
---
+++ 2ºTrimestre/practicas_segundo/aplicacion/vistas/ejemmodelos/tabla.php
@@ -0,0 +1,8 @@
+<?php
+
+$tabla = new CGrid($cabecera, $fill, ["class" => "tabla1"]);
+$paginado = new CPager($cabPag, []);
+
+//Dibija la tabla tantas veces como se haga el hecho
+echo $tabla->dibujate();
+echo $paginado->dibujate();
